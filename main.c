3cf31e0528afc1ac73b9a20eba71041f0f697fc9
stefys2000 <32293264+stefys2000@users.noreply.github.com>
2024-08-27 15:51:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,19 +20,8 @@ float64_t calculate_circle_area(int8_t radius) {
     return area;
 }
 
-float64_t calculate_circle_circumference(int8_t radius) {
-    float64_t circumference;
-    if(!check_valid_size(radius)) {
-        circumference = 0.0;
-    } else {
-        circumference = 2.0 * (float64_t)radius * M_PI;
-    }
-    return circumference;
-}
-
 main_return_t main(void) {
     const int8_t r = 1;
     calculate_circle_area(r);
-    calculate_circle_circumference(r);
     return 0;
 }
