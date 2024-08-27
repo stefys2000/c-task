3a9f1e06d27ff7cfae661bcee189433cc57b2af5
stefys2000 <32293264+stefys2000@users.noreply.github.com>
2024-08-27 15:51:33 +0200

    Update main.h

--- main.h
+++ main.h
@@ -12,6 +12,5 @@ typedef int main_return_t;
 main_return_t main(void);
 int8_t check_valid_size(int8_t checking_number);
 float64_t calculate_circle_area(int8_t radius);
-float64_t calculate_circle_circumference(int8_t radius);
 
 #endif
